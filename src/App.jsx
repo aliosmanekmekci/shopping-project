31cb1b77c7d279d5274296aa767ba569760604b2
ali osman ekmekci <aliosmanekmekci@gmail.com>
2023-12-10 22:15:20 +0300

    View all products added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
     <div className="container mx-auto mt-4 md:mt-10 lg:mt-15">
       <Header />
       <ProductList products={productList} />
+      <button className="mx-auto justify-center flex text-gray-800 font-light py-2 px-4 rounded mt-16 font-custom2 items-center opacity-50 cursor-not-allowed border-2">
+        View all Products
+      </button>
     </div>
   );
 }
